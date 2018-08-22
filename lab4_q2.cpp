62db481228f618ea4adef411039913aabd9b5f3e
ramyaranjansahoo <42238970+ramyaranjansahoo@users.noreply.github.com>
2018-08-22 16:52:56 +0530

    Add files via upload

---
+++ lab4_q2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//Declaring variables
+float f,c;
+//printing the operation
+cout <<"convert temperature in celcius into fahrenheit"<<endl;
+cout <<"enter temperature"<<endl;
+//Declaring variables
+cin>>f;
+c= (f-32)*5/9;
+
+//Printing result
+cout <<"temperature in fahrenheit is "<<c<<endl;
+return 0;
+}
---
+++ lab4_q6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//Declaring variables
+float f,c;
+//printing the operation
+cout <<"convert temperature from celcius into fahrenheit"<<endl;
+cout <<"enter temperature in celcius "<<endl;
+//Declaring variables
+cin>>c;
+f= (1.8*c)+32;
+
+//Printing result
+cout <<"temperature in fahrenheit is "<<f<<endl;
+return 0;
+}
